d4183fbc4eae76b2b76e1bae8adfb6f4bf8bdb83
hmanwani64 <harishmanwani1@gmail.com>
2026-08-28 09:55:06 -0700

    Initial Commit

---
+++ MacClean/MacCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacCleanApp.swift
+//  MacClean
+//
+//  Created by Harish on 8/28/26.
+//
+
+import SwiftUI
+
+@main
+struct MacCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
